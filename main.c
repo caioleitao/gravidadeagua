d2f3a7438f611152fb9fcdaf63c0072fd343e14c
Caio Leitão <kylespace@icloud.com>
2023-06-28 06:34:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-``` #include <stdio.h>
+#include <stdio.h>
 int main()
 {
     int tamLinha, tamColuna;
@@ -12,7 +12,7 @@ int main()
     }
     for (int linhaEscaneada = 0; linhaEscaneada < tamLinha; linhaEscaneada++) {
         for (int colunaEscaneada = tamColuna - 1; colunaEscaneada >= 0; colunaEscaneada--) {
-            // gravidade água para esquerda
+            // gravidade �gua para esquerda
             if (matriz[linhaEscaneada][colunaEscaneada] == 'o') {
                 if (linhaEscaneada != tamLinha - 1 && colunaEscaneada != 0) {
                     if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#') {
@@ -26,9 +26,9 @@ int main()
     }
     for (int linhaEscaneada = 0; linhaEscaneada < tamLinha; linhaEscaneada++) {
         for (int colunaEscaneada = 0; colunaEscaneada < tamColuna; colunaEscaneada++) {
-            // gravidade água para direta
+            // gravidade �gua para direta
             if (matriz[linhaEscaneada][colunaEscaneada] == 'o') {
-                if (linhaEscaneada != tamLinha - 1 && colunaEscaneada != tamColuna - 1) {
+                if (linhaEscaneada != tamLinha - 1 && colunaEscaneada != tamColuna) {
                     if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#') {
                         matriz[linhaEscaneada + 1][colunaEscaneada] = 'o';
                     } else {

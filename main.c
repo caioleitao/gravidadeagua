52c55b96b81513da6cac79598aeff59778d321ef
Caio Leitão <kylespace@icloud.com>
2023-06-28 15:15:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,29 +12,29 @@ int main()
     }
     for (int linhaEscaneada = 0; linhaEscaneada < tamLinha; linhaEscaneada++) {
         for (int colunaEscaneada = tamColuna - 1; colunaEscaneada >= 0; colunaEscaneada--) {
-            // gravidade �gua para esquerda
+            // gravidade agua para esquerda
             if (matriz[linhaEscaneada][colunaEscaneada] == 'o') {
-                if (linhaEscaneada != tamLinha - 1 && colunaEscaneada != 0) {
-                    if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#') {
+            
+                    if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#' && linhaEscaneada != tamLinha - 1 && colunaEscaneada != 0) {
                         matriz[linhaEscaneada + 1][colunaEscaneada] = 'o';
-                    } else {
+                    } else if(matriz[linhaEscaneada][colunaEscaneada - 1] != '#' && linhaEscaneada != tamLinha - 1 && colunaEscaneada != 0) {
                         matriz[linhaEscaneada][colunaEscaneada - 1] = 'o';
-                    }
+                    
                 }
             }
         }
     }
     for (int linhaEscaneada = 0; linhaEscaneada < tamLinha; linhaEscaneada++) {
         for (int colunaEscaneada = 0; colunaEscaneada < tamColuna; colunaEscaneada++) {
-            // gravidade �gua para direta
+            // gravidade agua para direta
             if (matriz[linhaEscaneada][colunaEscaneada] == 'o') {
-                if (linhaEscaneada != tamLinha - 1 && colunaEscaneada != tamColuna) {
-                    if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#') {
+              
+                    if (matriz[linhaEscaneada + 1][colunaEscaneada] != '#' && linhaEscaneada != tamLinha - 1 && colunaEscaneada != tamColuna) {
                         matriz[linhaEscaneada + 1][colunaEscaneada] = 'o';
-                    } else {
+                    } else if(matriz[linhaEscaneada][colunaEscaneada + 1] != '#' && linhaEscaneada != tamLinha - 1 && colunaEscaneada != tamColuna){
                         matriz[linhaEscaneada][colunaEscaneada + 1] = 'o';
                     }
-                }
+                
             }
         }
     }
